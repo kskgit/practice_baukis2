c1621c4e950c5b221cdd678726f7d5d2e8d6642c
keisuke <keisuke@diffeasy.com>
2020-08-22 23:00:56 +0900

    職員ページに別レイアウトをあてる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  def index
-    render action: "index"
+  layout :set_layout
+
+  private def set_layout
+    if params[:controller].match(%r{\A(staff|admin|customer)/})
+      Regexp.last_match[1]
+    else
+      "customer"
+    end
   end
 end
